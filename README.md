b6ec67cf9d0532be8acd810007de6b0b75ab88d7
Andres Godoy <andresegm@gmail.com>
2022-09-28 18:29:00 -0600

    Write event listeners for hovering mouse to change color, and clicking button to reset color. Added instructions to README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Etch-a-sketch
 Etch a sketch app from The Odin Project
+
+Instructions:
+Simply hover the nouse over the squares to draw them; click reset to set all squares back to white.
--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const container = document.querySelector('#container')
 
-const squareFactoryHor = () => {
-    for (let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.setAttribute('class', 'square');
-    container.appendChild(div);
-    }
+for (let i = 0; i < 256; i++) {
+    const sqr = document.createElement('div');
+    sqr.setAttribute('class', 'square'); //sets class, used in CSS for styling
+    container.appendChild(sqr);
+    sqr.addEventListener('mouseover', () => {
+        const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+        const r = randomBetween(0, 255);
+        const g = randomBetween(0, 255);
+        const b = randomBetween(0, 255);
+        const rgb = `rgb(${r},${g},${b})`   
+        sqr.style.backgroundColor=rgb;
+        const btn = document.querySelector('button')
+            btn.addEventListener('click', () => {
+            sqr.style.backgroundColor = 'transparent'
+        });
+  });
 };
-
-squareFactoryHor();
