a74d173f9cf359b9d09a76c4f4e39299d861314d
Andres Godoy <andresegm@gmail.com>
2022-09-30 09:39:15 -0600

    Organize code, take certain functions outside of createGrid function.

--- script.js
+++ script.js
@@ -1,19 +1,65 @@
-const container = document.querySelector('#container')
+const container = document.querySelector('.container')
 
-for (let i = 0; i < 256; i++) { //creates 256 divs (styled into squares in css)
-    const sqr = document.createElement('div');
-    sqr.setAttribute('class', 'square'); //sets class, used in CSS for styling
-    container.appendChild(sqr);
-    sqr.addEventListener('mouseover', () => {
-        const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1)); 
-        const r = randomBetween(0, 255);
-        const g = randomBetween(0, 255); //these produce a random color for the square
-        const b = randomBetween(0, 255);
-        const rgb = `rgb(${r},${g},${b})`   
-        sqr.style.backgroundColor=rgb;
+
+const createGrid = (n) => {
+  if (n < 10 || n > 100) {
+    alert('Please, make a valid selection');  //this block just checks to make sure number entered is more than 10 and less than 100
+    return;
+  }; 
+
+  for (let i = 0; i < n; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');     // this block creates rows
+        container.appendChild(row);
+
+    for (let j = 0; j < n; j++) {
+        const sqr = document.createElement('div');
+        sqr.classList.add('column'); // this block creates "columns" (more appropriately, squares)
+        sqr.addEventListener('mouseover', changeColor);
+        row.appendChild(sqr)
+        };
+    };
+}; 
+
+//this function removes all children elements, used for newGrid function
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {               
+      parent.removeChild(parent.firstChild);
+  }
+};   
+
+//creates new Grid (prompts user to input size)
+const newGrid = document.querySelector('.newGrid')
+newGrid.addEventListener('click', () => {   
+  removeAllChildNodes(container);
+  createGrid(prompt('Please input number of rows; must be a number between 10 and 100', 0));
+});
+
+//resets grid to white
+const reset = document.querySelector('.reset')
+reset.addEventListener('click', () => {
+    const containerDivs = container.querySelectorAll('div');
+    containerDivs.forEach(containerDivs => {
+        containerDivs.style.backgroundColor = "white";
     });
-    const btn = document.querySelector('button')
-    btn.addEventListener('click', () => {     // resets color to white when button is clicked
-    sqr.style.backgroundColor = 'transparent'
+})
+
+//toggles border style around squares between white and black
+const borderStyle = document.querySelector('.borderStyle')
+borderStyle.addEventListener('click', () => {   
+    const containerDivs = container.querySelectorAll('div');
+    containerDivs.forEach(containerDivs => {
+        containerDivs.classList.toggle('noBorder');
     });
-};
+});
+
+//Change color function
+const changeColor = (e) => {
+    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1)); 
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255); //these produce a random color for the square
+    const b = randomBetween(0, 255);
+    const rgb = `rgb(${r},${g},${b})`   
+    e.target.style.backgroundColor = rgb;
+    return;
+}; 
